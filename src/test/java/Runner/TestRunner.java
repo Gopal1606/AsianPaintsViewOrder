ff859ad2ecf4071cdf58ec9467235856940b94f1
ELCOT <ELCOT@Lenovo>
2021-05-05 18:04:30 +0530

    AsianPaints

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,15 +1,13 @@
 package Runner;
 
 
-
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Feature/ViewOrder.feature",glue= {"StepDefinition"})
-
-public class TestRunner {
-
+public class TestRunner extends AbstractTestNGCucumberTests
+{
+	
 }
+
+ 
--- src/test/java/StepDefinition/vieworder.java
+++ src/test/java/StepDefinition/vieworder.java
@@ -1,93 +1,107 @@
 package StepDefinition;
 
 
+import org.junit.Assert;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import AsianPaintsPOM.ViewOrderPOM;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 
 public class vieworder {
+	
 	WebDriver driver;
-	@Given("User is on Home Page")
-	public void user_is_on_home_page() {
-		
+	@BeforeTest
+	@Given("^User is on Home Page$")
+	public void user_is_on_Home_Page() throws Throwable {
 		System.setProperty("webdriver.chrome.driver","D:\\Selenium Software\\chromedriver_win32\\chromedriver.exe");
 	     driver = new ChromeDriver();
 	    driver.get("https://www.asianpaints.com/");
 	 
 	    driver.manage().window().maximize();
 	}
-
-	@When("User Navigate to Profile icon")
-	public void user_navigate_to_profile_icon() {
-		 ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
+    @Test(priority =1)
+	@When("^User Navigate to Profile icon$")
+	public void user_Navigate_to_Profile_icon() throws Throwable {
+		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
 		 orderpage.clickprofile();
-
 	}
-
-	@When("Click on login")
-	public void click_on_login() {
+    @Test(priority =2)
+	@When("^Click on login$")
+	public void click_on_login() throws Throwable {
 		 ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
-		orderpage.clickloginbtn();
+			orderpage.clickloginbtn();
 	}
-
-	@When("Enter the phone number")
-	public void enter_the_phone_number() {
-		 ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
+    @Test(priority =3)
+	@When("^Enter the phone number$")
+	public void enter_the_phone_number() throws Throwable {
+		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
 		 orderpage.enterno("8610425963");
 	}
-
-	@When("Click on submit")
-	public void click_on_submit() throws InterruptedException {
+	 @Test(priority =4)
+	@When("^Click on submit$")
+	public void click_on_submit() throws Throwable {
 		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
 		  orderpage.numbersubmit();
 		    Thread.sleep(20000);
 	}
-
-	@When("Click on submit otp")
-	public void click_on_submit_otp() throws InterruptedException {
+	 @Test(priority =5)
+	@When("^Click on submit otp$")
+	public void click_on_submit_otp() throws Throwable {
 		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
 		 orderpage.otpsubmit();
 		 Thread.sleep(10000);
 	}
-
-	@When("Click on profile icon")
-	public void click_on_profile_icon() {
+	 @Test(priority =6)
+	@When("^Click on profile icon$")
+	public void click_on_profile_icon() throws Throwable {
 		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
 		orderpage.clickprofile1();
 	}
-
-	@When("Click on my profile")
-	public void click_on_my_profile() {
+	 @Test(priority =7)
+	@When("^Click on my profile$")
+	public void click_on_my_profile() throws Throwable {
 		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
 		 orderpage.clickmyprofile();
 	}
-
-	@When("Click on my order tab")
-	public void click_on_my_order_tab() {
+	 @Test(priority =8)
+	@When("^Click on my order tab$")
+	public void click_on_my_order_tab() throws Throwable {
 		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
 		 orderpage.clickmyorder();
 	}
-
-	@When("User cant able to view ordered items")
-	public void user_cant_able_to_view_ordered_items() {
+	 @Test(priority =9)
+	@When("^User cant able to view ordered items$")
+	public void user_cant_able_to_view_ordered_items() throws Throwable {
 		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
+		boolean view =driver.findElement(By.xpath("//*[@id=\"emptyOrderTab\"]/div/div")).isDisplayed();
+		Assert.assertTrue(view);
 		   orderpage.clickprofile2();
 	}
-
-	@When("Click on signout")
-	public void click_on_signout() {
+	 @Test(priority =10)
+	@When("^Click on signout$")
+	public void click_on_signout() throws Throwable {
 		ViewOrderPOM orderpage = PageFactory.initElements(driver,ViewOrderPOM.class);
 		orderpage.signout();
+		   Thread.sleep(3000);
+		   
+		
 	}
-
-	@Then("User should view home page")
-	public void user_should_view_home_page() {
-	    driver.close();
-	}
+   @AfterTest
+	@Then("^User should view home page$")
+	public void user_should_view_home_page() throws Throwable {
+	  
+		  driver.close();
+	    }
+	
 
 }
